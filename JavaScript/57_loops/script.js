672b632f482539b8a9905298556c8ce68b3b193b
Tanishka Raghuwanshi <tanishka.raghuwanshi09@gmail.com>
2026-02-19 13:49:09 +0530

    JS loops

---
+++ JavaScript/57_loops/script.js
@@ -0,0 +1,43 @@
+console.log("loops")
+
+// let a=1;
+// // console.log(a)
+// // console.log(a+1)
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i );
+    
+// }
+
+// let obj ={
+//     name:"tanishka",
+//     role:"job",
+//     cp:"qwer"
+// }
+
+// // forin  prints key,element
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element)  
+// }
+
+// forof  iterator t a n i s h k a
+// for (const c of "tanishka") {
+//     console.log(c)  
+// }
+
+// while
+//  let i=0;
+//  while(i<6){
+//     console.log(i)
+//     i++;
+//  }
+
+//  do while
+ let i=10;
+do {
+     console.log(i)
+    i++;
+    
+} while (i<6);
+
